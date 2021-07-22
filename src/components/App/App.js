4c6b21b420249a59c57509ea48300f2762725d67
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-07-22 18:21:22 +0700

    feat/ wrote the code for promo component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Header } from '../Header/Header';
 import { Main } from '../Main/Main';
+import { Promo } from '../Promo/Promo';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Promo />
       <Main />
     </div>
   );
