4d498c090860b5b5b3a96fda609c7d15220ca4d2
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-07-29 21:59:13 +0700

    Added routes necessary for work

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,21 @@ function App() {
       <Route exact path="/">
         <Main />
       </Route>
+      <Route path="/movies">
+        <p>Фильмы</p>
+      </Route>
+      <Route path="/saved-movies">
+        <p>Сохранённые фильмы</p>
+      </Route>
+      <Route path="/profile">
+        <p>Cтраница с профилем пользователя</p>
+      </Route>
+      <Route path="/signin">
+        <p>Авторизация</p>
+      </Route>
+      <Route path="/signup">
+        <p>Регистрация</p>
+      </Route>
       <Route path="*">
         <PageNotFound />
       </Route>
