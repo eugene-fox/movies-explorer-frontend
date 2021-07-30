5a06c47f2fcc89831f35d73b587d8f56382fe858
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-07-31 02:40:52 +0700

    feat/ added burger menu icon

---
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -0,0 +1,9 @@
+import './BurgerMenu.css'
+
+export const BurgerMenu = () => {
+  return (
+   <div className="burger-menu__icon">
+     <span className="burger-menu__line"></span>
+   </div>
+  )
+}
