1dd06db5210417944e8af258ed146a4bfe277e59
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-08-03 05:53:13 +0700

    feat/ added cross

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react';
+import { useRouteMatch } from "react-router-dom";
 
 import './MoviesCard.css';
 
 export const MoviesCard = ({imageUrl, title, filmDuration, isSaved}) => {
+
+  const isSavedMoviesPage = useRouteMatch({ path: '/saved-movies', exact: true });
 
   const [filmLikeStatus, setFilmLikeStatus] = useState(isSaved);
 
@@ -20,12 +23,17 @@ export const MoviesCard = ({imageUrl, title, filmDuration, isSaved}) => {
       <div className="movies-card__description">
         <h2 className="movies-card__header">{title}</h2>
         <span className="movies-card__film-duration">{filmDuration}</span>
+        {isSavedMoviesPage ? (<button
+            type="button"
+            aria-label="Удалить фильм"
+            className="movies-card__delete-button"
+          />) : (
         <button
             type="button"
             aria-label="Сохранить фильм"
             className={filmLikeButtonClassName}
             onClick={filmLikeButtonClickHandler}
-          />
+          />)}
       </div>
     </li>
   )
