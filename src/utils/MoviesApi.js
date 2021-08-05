1be34f04a081f96e11e170db1cc72ffdcf9e5238
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-08-06 00:03:28 +0700

    feat/ created MoviesApi

---
+++ src/utils/MoviesApi.js
@@ -0,0 +1,35 @@
+import {
+  BEATFILM_MOVIES_URL
+} from './constants';
+
+export default class MoviesApi {
+  constructor(url) {
+    this._url = url;
+  }
+
+  //Установка заголовков запроса
+  _getHeaders() {
+    return {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  //Проверка ответа сервера
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  //Получение всех фильмов со стороннего API
+  getMovies() {
+    return fetch(this._url, {
+        method: 'GET',
+        headers: this._getHeaders()
+      })
+      .then((res) => this._checkResponse(res));
+  }
+}
+
+export const moviesApi = new MoviesApi(BEATFILM_MOVIES_URL);
