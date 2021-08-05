1821ef142ae789e1c3a2d7dffef48fb812f33108
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-08-05 22:59:31 +0700

    feat/ aaded current user context in app.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,43 +1,55 @@
 import './App.css';
+
+import { useEffect, useState } from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import { Main } from '../Main/Main';
 import { Register } from '../Register/Register';
 import { PageNotFound } from '../PageNotFound/PageNotFound';
-import { Route, Switch } from 'react-router-dom';
 import { Login } from '../Login/Login';
 import { Movies } from '../Movies/Movies';
 import { SavedMovies } from '../SavedMovies/SavedMovies';
 import { Profile } from '../Profile/Profile';
 
+import moviesApi from '../../utils/MoviesApi';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+
 //Временное переключение авторизации пользователя
 const isLoginIn = true;
 
 function App() {
+
+  //Стейт данных текущего пользователя
+  const [currentUser, setCurrentUser] = useState({});
+
   return (
-    <div className="app">
-      <Switch>
-      <Route exact path="/">
-        <Main isLoginIn={isLoginIn}/>
-      </Route>
-      <Route path="/movies">
-        <Movies isLoginIn={isLoginIn} />
-      </Route>
-      <Route path="/saved-movies">
-        <SavedMovies isLoginIn={isLoginIn}/>
-      </Route>
-      <Route path="/profile">
-        <Profile isLoginIn={isLoginIn}/>
-      </Route>
-      <Route path="/signin">
-        <Login />
-      </Route>
-      <Route path="/signup">
-        <Register />
-      </Route>
-      <Route path="*">
-        <PageNotFound />
-      </Route>
-      </Switch>
-    </div>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="app">
+        <Switch>
+          <Route exact path="/">
+            <Main isLoginIn={isLoginIn} />
+          </Route>
+          <Route path="/movies">
+            <Movies isLoginIn={isLoginIn} />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies isLoginIn={isLoginIn} />
+          </Route>
+          <Route path="/profile">
+            <Profile isLoginIn={isLoginIn} />
+          </Route>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Register />
+          </Route>
+          <Route path="*">
+            <PageNotFound />
+          </Route>
+        </Switch>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
