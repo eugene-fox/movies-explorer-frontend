4d8d5d4b5d7a0f16e624a9af26697dc94551150d
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-08-12 00:17:30 +0700

    added new constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,10 @@
 export const BASE_URL = 'https://api.eugenefox.movies.nomoredomains.monster';
 export const BEATFILM_MOVIES_URL = 'https://api.nomoreparties.co';
+
+//Продолжительность короткометражного фильма 40 минут
+export const SHORT_MOVIE_DURATION = 40;
+
+//Базовые разрешения устройств
+export const MOBILE_WIDTH = 480;
+export const TABLET_WIDTH = 768;
+export const LAPTOP_WIDTH = 1280;
