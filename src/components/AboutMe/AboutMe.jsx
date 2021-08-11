58d4e2e34bb6324a93ff205101e179a6f3c39e46
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-08-12 01:25:10 +0700

    fix/ edit about text

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -13,9 +13,7 @@ export function AboutMe() {
           <div className="about-me__text-wrapper">
             <h3 className="about-me__name">Евгений</h3>
             <p className="about-me__short-description">Фронтенд-разработчик, 28 лет</p>
-            <p className="about-me__paragraph">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-              и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-
+            <p className="about-me__paragraph">Я родился и живу в России, закончил местный университет. Я люблю слушать музыку, играть на музыкальных инструментах. Делаю свои маленькие шаги к веб-разработке</p>
             <ul className="about-me__student-links">
               <li><a className="about-me__student-link-item" href="https://www.facebook.com/eu.lifesizemirror" target="_blank" rel="noreferrer">Facebook</a></li>
               <li><a className="about-me__student-link-item" href="https://github.com/eugene-fox" target="_blank" rel="noreferrer">Github</a></li>
