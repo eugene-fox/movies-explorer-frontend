6af4c45fae11cb6659458cd0057d983e427898d3
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-08-26 15:29:21 +0700

    Removed card templates

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -3,10 +3,6 @@ import { MoviesCard } from '../MoviesCard/MoviesCard';
 import { Preloader } from '../Preloader/Preloader';
 
 import picUrl1 from '../../images/pic-1.jpg';
-import picUrl2 from '../../images/pic-2.jpg';
-import picUrl3 from '../../images/pic-3.jpg';
-import picUrl4 from '../../images/pic-4.jpg';
-import picUrl5 from '../../images/pic-5.jpg';
 
 export const MoviesCardList = () => {
 
@@ -18,42 +14,6 @@ export const MoviesCardList = () => {
           <MoviesCard
             imageUrl={picUrl1}
             title="33 слова о дизайне"
-            filmDuration="1ч 42м"
-            isSaved={false}
-          />
-          <MoviesCard
-            imageUrl={picUrl2}
-            title="Киноальманах «100 лет дизайна»"
-            filmDuration="1ч 42м"
-            isSaved={true}
-          />
-          <MoviesCard
-            imageUrl={picUrl3}
-            title="В погоне за Бенкси"
-            filmDuration="1ч 42м"
-            isSaved={true}
-          />
-          <MoviesCard
-            imageUrl={picUrl4}
-            title="Баския: Взрыв реальности"
-            filmDuration="1ч 42м"
-            isSaved={false}
-          />
-          <MoviesCard
-            imageUrl={picUrl5}
-            title="Бег это свобода"
-            filmDuration="1ч 42м"
-            isSaved={false}
-          />
-          <MoviesCard
-            imageUrl={picUrl1}
-            title="Книготорговцы"
-            filmDuration="1ч 42м"
-            isSaved={true}
-          />
-          <MoviesCard
-            imageUrl={picUrl1}
-            title="Книготорговцы"
             filmDuration="1ч 42м"
             isSaved={false}
           />
