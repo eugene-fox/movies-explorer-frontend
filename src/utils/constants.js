6372ff351fcc60de8af49455ea37b6ac8cb63e9a
eugene-fox <69255033+eugene-fox@users.noreply.github.com>
2021-09-02 16:45:09 +0700

    added urls for api

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+// Адрес серверного API c авторизацией пользователями и сохраненными фильмами
+export const API_URL = 'https://api.eugenefox.movies.nomoredomains.monster';
+
+// Адрес API с фильмами "https://api.nomoreparties.co/beatfilm-movies"
+export const MOVIES_API_URL = 'https://api.nomoreparties.co';
